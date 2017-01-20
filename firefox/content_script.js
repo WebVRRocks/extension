4487ddac80bb40d901ab49b6548ef84c54c5799c
Christopher Van Wiemeersch <hearcomestreble@gmail.com>
2017-01-19 19:18:56 -0800

    get events working

--- firefox/content_script.js
+++ firefox/content_script.js
@@ -1,88 +1,96 @@
 /* global chrome */
 console.log('my awesome content_script');
 
-chrome.runtime.sendMessage(
-  {
-    action: 'hello world'
-  },
-  msg => {
-    console.log(msg);
-  }
-);
+alert(1);
+
+// chrome.runtime.sendMessage(
+//   {
+//     action: 'hello world'
+//   },
+//   msg => {
+//     console.log(msg);
+//   }
+// );
 
 
 
-class ObjectEventTarget {
-  constructor() {
-    if (this.initialized) {
-      return;
-    }
-    this.initialized = true;
-    const args = [];
-    for (let i = 1; i < arguments.length; ++i) {
-      args[i - 1] = arguments[i];
-    }
-  }
+// class ObjectEventTarget {
+//   constructor () {
+//     if (this.initialized) {
+//       return;
+//     }
+//     this.initialized = true;
+//     const args = [];
+//     for (let i = 1; i < arguments.length; ++i) {
+//       args[i - 1] = arguments[i];
+//     }
+//   }
 
-  addEventListener (type, callback, options) {
-    if (!this.initialized || !(this instanceof ObjectEventTarget)) {
-      throw new TypeError('Illegal invocation');
-    }
-    if (arguments.length < 2) {
-      throw new TypeError(`Failed to execute 'addEventListener' on 'EventTarget': 2 argument required, but only ${arguments.length} present.`);
-    }
-    const args = [];
-    for (let i = 0; i < arguments.length && i < 3; ++i) {
-      args[i] = arguments[i];
-    }
-    args[0] = conversions.DOMString(args[0]);
-    return Impl.interface.prototype.addEventListener.apply(this, args);
-  }
+//   addEventListener (type, callback, options) {
+//     if (!this.initialized || !(this instanceof ObjectEventTarget)) {
+//       throw new TypeError('Illegal invocation');
+//     }
+//     if (arguments.length < 2) {
+//       throw new TypeError(`Failed to execute 'addEventListener' on 'EventTarget': 2 argument required, but only ${arguments.length} present.`);
+//     }
+//     const args = [];
+//     for (let i = 0; i < arguments.length && i < 3; ++i) {
+//       args[i] = arguments[i];
+//     }
+//     args[0] = conversions.DOMString(args[0]);
+//     return Impl.interface.prototype.addEventListener.apply(this, args);
+//   }
 
-  removeEventListener (type, callback) {
-    if (!this.initialized || !(this instanceof ObjectEventTarget)) {
-      throw new TypeError('Illegal invocation');
-    }
-    if (arguments.length < 2) {
-      throw new TypeError(`Failed to execute 'removeEventListener' on 'EventTarget': 2 argument required, but only ${arguments.length} present.`);
-    }
-    const args = [];
-    for (let i = 0; i < arguments.length && i < 3; ++i) {
-      args[i] = arguments[i];
-    }
-    args[0] = conversions.DOMString(args[0]);
-    return Impl.interface.prototype.removeEventListener.apply(this, args);
-  }
+//   removeEventListener (type, callback) {
+//     if (!this.initialized || !(this instanceof ObjectEventTarget)) {
+//       throw new TypeError('Illegal invocation');
+//     }
+//     if (arguments.length < 2) {
+//       throw new TypeError(`Failed to execute 'removeEventListener' on 'EventTarget': 2 argument required, but only ${arguments.length} present.`);
+//     }
+//     const args = [];
+//     for (let i = 0; i < arguments.length && i < 3; ++i) {
+//       args[i] = arguments[i];
+//     }
+//     args[0] = conversions.DOMString(args[0]);
+//     return Impl.interface.prototype.removeEventListener.apply(this, args);
+//   }
 
-  dispatchEvent(event) {
-    if (!this.initialized || !(this instanceof ObjectEventTarget)) {
-      throw new TypeError('Illegal invocation');
-    }
-    if (arguments.length < 1) {
-      throw new TypeError(`Failed to execute 'dispatchEvent' on 'EventTarget': 1 argument required, but only ${arguments.length} present.`);
-    }
-    const args = [];
-    for (let i = 0; i < arguments.length && i < 1; ++i) {
-      args[i] = arguments[i];
-    }
-    return Impl.interface.prototype.dispatchEvent.apply(this, args);
-  }
-}
+//   dispatchEvent (event) {
+//     if (!this.initialized || !(this instanceof ObjectEventTarget)) {
+//       throw new TypeError('Illegal invocation');
+//     }
+//     if (arguments.length < 1) {
+//       throw new TypeError(`Failed to execute 'dispatchEvent' on 'EventTarget': 1 argument required, but only ${arguments.length} present.`);
+//     }
+//     const args = [];
+//     for (let i = 0; i < arguments.length && i < 1; ++i) {
+//       args[i] = arguments[i];
+//     }
+//     return Impl.interface.prototype.dispatchEvent.apply(this, args);
+//   }
+// }
 
-navigator.rocksVr = new ObjectEventTarget();
+// navigator.rocksVr = new ObjectEventTarget();
 
-navigator.getVRDisplays().then(displays => {
-  console.log('displays', displays);
-});
+// navigator.getVRDisplays().then(displays => {
+//   console.log('displays', displays);
+// });
 
 var logEvent = e => {
   console.log('[event]', e.type, e);
 };
 
+window.addEventListener('vrdisplayconnected', logEvent);
 window.addEventListener('vrdisplayconnect', logEvent);
+window.addEventListener('vrdisplaydisconnected', logEvent);
 window.addEventListener('vrdisplaydisconnect', logEvent);
 window.addEventListener('vrdisplayactivate', logEvent);
 window.addEventListener('vrdisplaydeactivate', logEvent);
 window.addEventListener('vrdisplayblur', logEvent);
 window.addEventListener('vrdisplayfocus', logEvent);
 window.addEventListener('vrdisplaypresentchange', logEvent);
+window.addEventListener('appinstalled', logEvent);
+window.addEventListener('load', logEvent);
+
+console.log('loaded');
--- firefox/options.js
+++ firefox/options.js
@@ -1,13 +1,13 @@
-function saveOptions (e) {
-  browser.storage.local.set({
-    vr_displays: document.querySelector('#vr_displays').value
-  });
-  e.preventDefault();
-}
-
-function loadOptions () {
-  var gettingItem = browser.storage.local.get('webvr:headset');
-  gettingItem.then(res => {
-    document.querySelector('#vr_displays').value = res.vr_displays;
-  });
-}
+// function saveOptions (e) {
+//   browser.storage.local.set({
+//     vr_displays: document.querySelector('#vr_displays').value
+//   });
+//   e.preventDefault();
+// }
+//
+// function loadOptions () {
+//   var gettingItem = browser.storage.local.get('webvr:headset');
+//   gettingItem.then(res => {
+//     document.querySelector('#vr_displays').value = res.vr_displays;
+//   });
+// }
